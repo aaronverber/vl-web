98fe15c3674d3a6f657d0e10a6c24253466efaa3
Stackbit Code Editor <projects@stackbit.com>
2021-05-17 06:36:01 +0000

    HeaderLinks.jsx: changed

--- src/components/Header/HeaderLinks.jsx
+++ src/components/Header/HeaderLinks.jsx
@@ -10,7 +10,7 @@ import ListItem from "@material-ui/core/ListItem";
 import Tooltip from "@material-ui/core/Tooltip";
 
 // @material-ui/icons
-import { Apps, Work, People, EmailIcon } from "@material-ui/icons";
+import { Apps, Work, People, Email } from "@material-ui/icons";
 
 // React icons
 import { FaTwitter, FaFacebook, FaInstagram } from 'react-icons/fa';
@@ -52,7 +52,7 @@ function HeaderLinks({ ...props }) {
           target="_blank"
           className={classes.navLink}
         >
-          <EmailIcon className={classes.icons} /> Contact
+          <Email className={classes.icons} /> Contact
         </Button>
       </ListItem>
       
