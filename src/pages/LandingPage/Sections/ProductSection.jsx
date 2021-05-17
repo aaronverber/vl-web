f0ff767265c43bace2e5a753c04325e9d93e0771
Stackbit Code Editor <projects@stackbit.com>
2021-05-17 18:49:34 +0000

    ProductSection.jsx: changed

--- src/pages/LandingPage/Sections/ProductSection.jsx
+++ src/pages/LandingPage/Sections/ProductSection.jsx
@@ -20,7 +20,7 @@ class ProductSection extends React.Component {
       <div className={classes.section}>
         <GridContainer justify="right">
           <GridItem xs={12} sm={12} md={8}>
-            <h1 className={classes.title} styles={"color:#fff; font-family: 'Saira Semi Condensed'"}>VANNEVAR Decrypt</h1>
+            <h1 className={classes.decrypt} styles={"color:#fff; font-family: 'Saira Semi Condensed'"}>VANNEVAR Decrypt</h1>
             <h2>
               A persistent, taskable sensor for foreign text data.
             </h2>
