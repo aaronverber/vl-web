bc556f525899c91962422557e6e4352f445e1dec
Stackbit Code Editor <projects@stackbit.com>
2021-05-17 06:32:27 +0000

    HeaderLinks.jsx: changed

--- src/components/Header/HeaderLinks.jsx
+++ src/components/Header/HeaderLinks.jsx
@@ -42,7 +42,7 @@ function HeaderLinks({ ...props }) {
           target="_blank"
           className={classes.navLink}
         >
-          <Work className={classes.icons} /> Careers
+          <Apps className={classes.icons} /> Careers
         </Button>
       </ListItem>
       <ListItem className={classes.listItem}>
