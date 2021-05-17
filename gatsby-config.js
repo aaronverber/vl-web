5328a92c7c7dac4f79d5ceddc8215f074e745f2a
Stackbit Code Editor <projects@stackbit.com>
2021-05-17 06:53:05 +0000

    gatsby-config.js: changed

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   pathPrefix: `/mini-gatsbyv2-material-kit-react`,
   siteMetadata: {
-    title: 'Vannevar Labs Test',
+    title: 'Vannevar Labs',
   },
   plugins: [
     'gatsby-plugin-resolve-src',
