9453ffc8f9bcbebb9f9b6a07d84d0e5c043d983b
aaronverber <aaron.verber@gmail.com>
2021-05-20 13:23:57 -0700

    router fix attempt

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,10 @@
 import React from "react";
-import {createMemoryHistory} from "history";
-import {Route, Router, Switch} from "react-router-dom";
 
 import "assets/scss/material-kit-react.scss?v=1.4.0";
 // pages for this product
 import LandingPage from "./LandingPage/LandingPage.jsx";
 
-let hist = createMemoryHistory();
+//let hist = createMemoryHistory();
 
 export default () => (
         <LandingPage />
--- src/pages/people.js
+++ src/pages/people.js
@@ -1,16 +1,13 @@
 import React from "react";
-import {createMemoryHistory} from "history";
-import {Route, Router} from "react-router-dom";
 
 import "assets/scss/material-kit-react.scss?v=1.4.0";
 // pages for this product
 import People from "./People.jsx";
 
-let hist = createMemoryHistory();
+//let hist = createMemoryHistory();
 
 console.log("This is a test that it read people.js. It did!");
 
 export default () => (
-
         <People />
 );
